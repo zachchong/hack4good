552591c43a38e2aadcb952cb6b8ad2778d664736
SoyWater <ivanlxy123@gmail.com>
2025-01-14 15:06:07 +0800

    added categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,20 +2,26 @@ import './categories.css'
 
 function Categories() {
   return (
-    <div>
-      <ul class = "categories">
-        <li>
-            <select>
-                <option>Electronics</option>
-                <option>Electronics</option>
-                <option>Electronics</option>
-            </select>
-        </li>
-        <li>Groceries</li>
-        <li>Clothes</li>
-        <li>Murder</li>
-      </ul>
-    </div>
+    <nav className = "cats">
+        <div className = "categories">
+        <input type="checkbox" name="" id=""></input>
+          <div class="hamburger-lines">
+              <span class="line line1"></span>
+              <span class="line line2"></span>
+              <span class="line line3"></span>
+          </div>
+          <ul class="menu-items">
+              <li><a >Groceries</a></li>
+              <li><a>Electronics</a></li>
+              <li><a >Fashion</a></li>
+              <li><a >Drugs</a></li>
+              <li><a >Weaponry</a></li>
+              <li><a>Dinosaur</a></li>
+          </ul>
+
+        </div>
+    </nav>
+
   );
 }
 
