8ca687dbb2ba44bd22a8ba9787e8fd8273953c94
SoyWater <ivanlxy123@gmail.com>
2025-01-14 14:24:17 +0800

    yes

---
+++ src/components/Categories.jsx
@@ -0,0 +1,22 @@
+import './categories.css'
+
+function Categories() {
+  return (
+    <div>
+      <ul class = "categories">
+        <li>
+            <select>
+                <option>Electronics</option>
+                <option>Electronics</option>
+                <option>Electronics</option>
+            </select>
+        </li>
+        <li>Groceries</li>
+        <li>Clothes</li>
+        <li>Murder</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
