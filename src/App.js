6755d48fca98ebf9725ea6741d13450d3aab1c6c
SoyWater <ivanlxy123@gmail.com>
2025-01-14 21:16:29 +0800

    mui update for categories

--- src/App.js
+++ src/App.js
@@ -4,12 +4,85 @@ import Categories from './components/Categories';
 import Navbar from './components/Navbar';
 
 
+
 function App() {
   return (
     <div className="App">
       <Navbar /> 
       <Categories />
+      <ul>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
 
+      </ul>
     </div>
   );
 }
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,27 +1,49 @@
 import './categories.css'
+import {Tabs, Tab, Divider} from '@mui/material/';
+import * as React from 'react';
+
+
+
 
 function Categories() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
-    <nav className = "cats">
-        <div className = "categories">
-        <input type="checkbox" name="" id=""></input>
-          <div class="hamburger-lines">
-              <span class="line line1"></span>
-              <span class="line line2"></span>
-              <span class="line line3"></span>
-          </div>
-          <ul class="menu-items">
-              <li><a >Groceries</a></li>
-              <li><a>Electronics</a></li>
-              <li><a >Fashion</a></li>
-              <li><a >Drugs</a></li>
-              <li><a >Weaponry</a></li>
-              <li><a>Dinosaur</a></li>
-          </ul>
+    <div>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="scrollable"
+        scrollButtons="auto"
+        aria-label="scrollable auto tabs example"
+      >
+        <Tab label="Item One" />
+        <Tab label="Item Two" />
+        <Tab label="Item Three" />
+        <Tab label="Item Four" />
+        <Tab label="Item Five" />
+        <Tab label="Item Six" />
+        <Tab label="Item Seven" />
+        <Tab label="Item Eight" />
+        <Tab label="Item Nine" />
+        <Tab label="Item Ten" />
+        <Tab label="Item Eleven" />
+        <Tab label="Item Twelve" />
+        <Tab label="Item Thirteen" />
+        <Tab label="Item Fourteen" />
+        <Tab label="Item Fifteen" />
+        <Tab label="Item Sixteen" />
+        <Tab label="Item Seventeen" />
+        <Tab label="Item Eighteen" />
+      </Tabs>
 
-        </div>
-    </nav>
+    </div>
 
+
+  
   );
 }
 
